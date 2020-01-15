e4db50e8e5bd0911e2fca215e12417302e332b2b
ElizabethFultz <efultz@bellarmine.edu>
2020-01-15 18:14:44 -0500

    Inital Commit

---
+++ Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sphere_EAF
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Sphere sphere1 = new Sphere();
+            Console.WriteLine("sphere1: " + sphere1.ToString());
+
+            Sphere sphere2 = new Sphere(5.2);
+            Console.WriteLine("The diameter of sphere2 is: " + sphere2.GetDiameter() + "\nThe volume of sphere2 is: " + 
+                sphere2.GetVolume() + "\nThe surface area of sphere2 is: " + sphere2.GetSurfaceArea());
+
+            Console.WriteLine("\nPress Any Key to Continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sphere.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * This class models a sphere object
+ * @author Rob Kelley
+ * @version 1.0
+ * Lab02-Solution
+ * SP19
+ */
+
+namespace Sphere_EAF
+{
+    class Sphere
+    {
+
+		private double diameter;
+
+		/**
+		 * Empty-argument constructor to put object
+		 * into a consistent state.
+		 */
+		public Sphere()
+		{
+			diameter = 2.0;
+		}//end constructor
+
+		/**
+		 * Constructor accepts value of diameter.
+		 * Preferred constructor.
+		 * @param diameter
+		 */
+		public Sphere(double diameter)
+		{
+
+			this.SetDiameter(diameter);
+
+		}//end constructor
+
+		/**
+		 * Calculates the volume of the sphere.
+		 * @return volume
+		 */
+		public double GetVolume()
+		{
+
+			return (4.0 / 3.0) * Math.PI * Math.Pow(diameter / 2, 3);
+
+		}//end GetVolume
+
+		/**
+		 * Calculates the surface area of the sphere.
+		 * @return - surface area
+		 */
+		public double GetSurfaceArea()
+		{
+
+			return 4.0 * Math.PI * Math.Pow(diameter / 2, 2);
+		}//end GetSurfaceArea
+
+
+		/**
+		 * Getter for diameter
+		 * @return
+		 */
+		public double GetDiameter()
+		{
+			return diameter;
+		}//end GetDiameter
+
+		/**
+		 * Setter for diameter. Checks bound on
+		 * formal parameter and resets to 2 if
+		 * the value is less than 2.
+		 * @param diameter
+		 */
+		public void SetDiameter(double diameter)
+		{
+			if (diameter < 2)
+				this.diameter = 2;
+			else
+				this.diameter = diameter;
+		}//end SetDiameter
+
+		
+	public override String ToString()
+		{
+			return "Sphere [diameter=" + diameter + "]";
+		}//end toString
+
+	}
+}
